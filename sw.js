4d82e3e09fb6df6b992678cd4442aa31d4008bae
qibrahim123 <ibbiq5000@gmail.com>
2026-09-05 01:23:36 -0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'jotwell-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.ok && new URL(e.request.url).origin === location.origin) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
